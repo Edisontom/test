b1e8843cbf1161b976567199e73421d1f34e5399
Edisontom <romazinchuk@gmail.com>
2016-07-12 23:38:10 +0300

    Add files via upload

---
+++ calc3.py
@@ -0,0 +1,19 @@
+number1=float(input("Please enter first number:"))
+action=str(input("Enter '+', '-', '*' or '/':"))
+number2=float(input("Please enter second number:"))
+
+calc = {
+    "+": number1 + number2,
+    "-": number1 - number2,
+    "*": number1 * number2,
+    "/": number1/number2
+
+
+}
+
+if calc == "/" and number2 ==0:
+    print ("Error,  divide to '0'")
+else:
+    print(calc.get(action))
+
+    #I need help with divide on zero "0".
